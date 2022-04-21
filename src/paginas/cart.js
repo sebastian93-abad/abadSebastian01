ff1877f6dfbd22bfdbd8d1c22dbc27fba22ff298
sebastian abad <sebastian93.abad@gmail.com>
2022-04-21 19:46:35 -0300

    trabajando ...

--- src/paginas/cart.js
+++ src/paginas/cart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CartContext from '../context/cartContext';
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Cart = () => {
@@ -8,22 +9,47 @@ const Cart = () => {
     console.log('pantalla del producto agregado:', cartProducts )
 
     return(
-        cartProducts.map( (cartProduct) =>{
-            const {titulo, talle, precio, id} = cartProduct
-            return(
-                    <div className="carta" key={id}>
-                        <h2>{titulo}</h2>
-                        <p>Talle:{talle}</p>
-                        <p>Precio : $ {precio}</p>
-                        <button onClick={() => eliminarProducto(cartProduct)}>Eliminar</button>
-                    </div>
+        <div>
+            {
+                (cartProducts.length === 0)
+
+                &&
+
+                <div>
+                    <p>No hay items en tu carrito...</p>
+                    <button><Link to={'/'}>VOLVER A COMPRAR</Link></button>
+                </div>
+
+            }
+            
+            
+            
+            {
+
+                cartProducts.map( (cartProduct) =>{
+                    const {titulo, talle, precio, id} = cartProduct
+                    return(
+                        <div className="carta" key={id}>
+                            <h2>{titulo}</h2>
+                            <p>Talle:{talle}</p>
+                            <p>Precio : $ {precio}</p>
+                            <button onClick={() => eliminarProducto(cartProduct)}>Eliminar</button>
+                        </div>
+            
                 
+                    )
+            
+
+    
+                })
                     
-            )
+            }
+            
                 
 
         
-        })
+        </div>
+        
 
     )
 
